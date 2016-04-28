79a0d74c334bf8b373fa5c89a5ecdf07dc812ed5
miaoski <lin.zhemin@gmail.com>
2016-04-29 01:52:01 +0800

    fixed several lexemes with Chinese

--- txt/makeindex.py
+++ txt/makeindex.py
@@ -7,14 +7,22 @@ import json
 
 INDEX = []
 
+def strip_brakets(s):
+    p = s.find('(')
+    if p == -1:
+        return s
+    else:
+        return s[:p]
+
 title = None
 state = None
-for fn in glob.iglob('?.txt'):
+for fn in glob.iglob('*.txt'):
     fp = open(fn)
+    print fn
     for line in fp:
         l = line.strip()
         if l == '' and title:           # 寫入詞條
-            INDEX.append(title.replace('g', 'ng'))
+            INDEX.append(title)
             title = None
             state = None
             continue
@@ -25,16 +33,21 @@ for fn in glob.iglob('?.txt'):
         xs = l.split()              # 處理 word'a = word'b
         if state is None and len(xs) == 3 and xs[1] == '=':
             title = xs[0].strip()
-            INDEX.append(title.replace('g', 'ng'))
+            INDEX.append(title)
             title = None
             continue
         if state is None:           # 詞
-            title = l.strip()
+            title = strip_brakets(l)
+            if title == '':
+                print 'Wrong!', l
             state = 't'
 INDEX.append(title)
 
 f = codecs.open('index.json', mode='w', encoding='utf8')
 x = json.dumps(INDEX, indent=2, separators=(',', ':'), ensure_ascii = False)
-# print x[55260:55280]
-f.write(x)
+try:
+    f.write(x)
+except UnicodeDecodeError as e:
+    print x[e.start-20:e.start+20]
+    raise
 f.close()
