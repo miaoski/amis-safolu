30d877f1de1dcb56281cda9ca7c61a1839ab6011
wildjcrt <wildjcrt@yahoo.com.tw>
2016-06-18 19:43:13 +0800

    新增詞幹的檢查程式與對應詞彙程式
    新增 2 個檔案:
    * txt/stem-checker.rb
    * tmp/amis-stem-words.json

---
+++ txt/stem-checker.rb
@@ -0,0 +1,52 @@
+# 目的：檢查 txt 是否有標注詞幹 stem，但卻沒有這個詞的定義
+# 運行方式：在終端機下執行命令 $ ruby txt/stem-checker.rb
+# 結果：執行命令後，會輸出結果在 tmp/缺少定義的詞幹.txt
+
+require "pry"
+require "json"
+
+@words = []
+@stems = []
+
+Dir.glob("txt/*.txt").each do |filename|
+  next if filename.include?("24-")
+
+  lines = File.readlines(filename)
+  terms = []
+  empty_line = true
+  lines.each do |line|
+    line.strip!
+    if empty_line
+      terms << line
+      empty_line = false
+    end
+    if line == ""
+      empty_line = true
+    end
+  end
+
+  words = []
+  stems = []
+  terms.each do |term|
+    word, stem = term.sub(")", "").split("(")
+    words << word
+    stems << stem if stem
+  end
+
+  @words += words
+  @stems += stems.uniq
+end
+
+missed = []
+@stems.each do |stem|
+  unless @words.include? stem
+    missed << stem
+  end
+end
+missed.uniq!
+
+File.open("tmp/缺少定義的詞幹.txt", "w") do |file|
+  missed.each do |term|
+    file.puts term
+  end
+end
---
+++ txt/stem-mapping.rb
@@ -0,0 +1,38 @@
+# 目的：根據 txt 產生 stem to words 的對應表，格式為 JSON。會用在萌典搜尋列找出詞彙的排序。
+# 運行方式：在終端機下執行命令 $ ruby txt/stem-mapping.rb
+# 結果：執行命令後，會輸出結果在 tmp/amis-stem-words.json
+
+require "json"
+
+@stems = {}
+Dir.glob("txt/*.txt").each do |filename|
+  next if filename.include?("24-")
+
+  lines = File.readlines(filename)
+  terms = []
+  empty_line = true
+  lines.each do |line|
+    line.strip!
+    if empty_line
+      terms << line
+      empty_line = false
+    end
+    if line == ""
+      empty_line = true
+    end
+  end
+
+  terms.each do |term|
+    word, stem = term.sub(")", "").split("(")
+    if stem
+      @stems[stem] ||= []
+      @stems[stem] << word
+    end
+  end
+end
+
+@stems = @stems.inject({}) {|hash, stem| hash[stem[0]] = stem[1].sort; hash}
+
+File.open("tmp/amis-stem-words.json", "w") do |file|
+  file.puts JSON.pretty_generate(@stems)
+end
