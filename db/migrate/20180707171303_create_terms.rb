13673ebf017048b355454898bfba32bf7c94fe23
wildjcrt <wildjcrt@yahoo.com.tw>
2018-07-08 10:03:25 +0800

    Migrate to generate stems, terms and raw_contents tables

---
+++ db/migrate/20180707171303_create_terms.rb
@@ -0,0 +1,15 @@
+class CreateTerms < ActiveRecord::Migration[5.0]
+  def up
+    create_table :terms do |t|
+      t.string  :name
+      t.integer :stem_id
+      t.timestamps
+    end
+
+    add_index :terms, :stem_id
+  end
+
+  def down
+    drop_table :terms
+  end
+end
