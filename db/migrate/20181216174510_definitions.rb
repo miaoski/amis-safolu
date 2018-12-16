e282cde5102b79b6fd530d1831eae391c0f4d42d
wildjcrt <wildjcrt@yahoo.com.tw>
2018-12-16 17:55:31 +0800

    Create Definition, Example and Alternative models

---
+++ db/migrate/20181216174510_definitions.rb
@@ -0,0 +1,27 @@
+class Definitions < ActiveRecord::Migration[5.0]
+  def up
+    create_table :definitions do |t|
+      t.integer :term_id
+      t.string  :content
+      t.timestamps
+    end
+
+    create_table :examples do |t|
+      t.integer :definition_id
+      t.string  :content
+      t.timestamps
+    end
+
+    create_table :alternatives do |t|
+      t.integer :definition_id
+      t.string  :content
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :definitions
+    drop_table :examples
+    drop_table :alternatives
+  end
+end
