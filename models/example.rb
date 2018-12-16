2b460c9dd2e820acc087e1417a671cc4df629e29
wildjcrt <wildjcrt@yahoo.com.tw>
2018-12-16 17:55:31 +0800

    Create Definition, Example and Alternative models

---
+++ models/example.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: examples
+#
+#  id            :integer          not null, primary key
+#  definition_id :integer
+#  content       :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+class Example < ApplicationRecord
+  belongs_to :definition
+end
