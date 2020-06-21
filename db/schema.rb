f4e32a152c4d443a53746c5526d16ee2267a128f
wildjcrt <wildjcrt@yahoo.com.tw>
2020-06-21 14:08:19 +0800

    Migrate to add term_type in Synonym model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_182905) do
+ActiveRecord::Schema.define(version: 2020_06_21_140234) do
 
   create_table "definitions", force: :cascade do |t|
     t.integer "term_id"
