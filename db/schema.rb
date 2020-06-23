62aa1f0a5dade46e480acf2e185a433c5b136054
wildjcrt <wildjcrt@yahoo.com.tw>
2020-06-23 10:12:48 +0800

    Re-create db/amis-sqlite3-db.sqlite3 and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2020_06_21_140234) do
   create_table "definitions", force: :cascade do |t|
     t.integer "term_id"
     t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "descriptions", force: :cascade do |t|
@@ -29,23 +29,23 @@ ActiveRecord::Schema.define(version: 2020_06_21_140234) do
   create_table "examples", force: :cascade do |t|
     t.integer "definition_id"
     t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "raw_contents", force: :cascade do |t|
     t.string "key"
     t.string "value"
     t.integer "term_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["term_id"], name: "index_raw_contents_on_term_id"
   end
 
   create_table "stems", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "synonyms", force: :cascade do |t|
@@ -53,13 +53,14 @@ ActiveRecord::Schema.define(version: 2020_06_21_140234) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "term_type", limit: 5
   end
 
   create_table "terms", force: :cascade do |t|
     t.string "name"
     t.integer "stem_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.integer "repetition"
     t.index ["stem_id"], name: "index_terms_on_stem_id"
   end
