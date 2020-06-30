d2c01d8ae0ea7546630adba76d8945d958d9215a
wildjcrt <wildjcrt@yahoo.com.tw>
2020-06-30 08:23:40 +0800

    Correct to use \ufffa and \ufffb in generate-moedict-json.rb
    Re-generate s/revdict-amis-def.txt and s/revdict-amis-ex.txt

--- generate-moedict-json.rb
+++ generate-moedict-json.rb
@@ -24,26 +24,34 @@ def stem_words_json
 end
 
 # revdict-amis-def.txt
+# 萌典前端使用的標記
+# \ufff9: 阿美語例句
+# \ufffa: 英文例句
+# \ufffb: 漢語例句
 def revdict_amis_def_txt
   File.open('s/revdict-amis-def.txt', 'w') do |file|
     Term.includes(:descriptions).find_each do |term|
       content = term.descriptions.pluck(:content).join
-      file.puts "#{term.name}#{content}"
+      file.puts "\ufffa#{term.name}\ufffb#{content}"
     end
   end
 end
 
 # revdict-amis-ex.txt
+# 萌典前端使用的標記
+# \ufff9: 阿美語例句
+# \ufffa: 英文例句
+# \ufffb: 漢語例句
 def revdict_amis_ex_txt
   File.open('s/revdict-amis-ex.txt', 'w') do |file|
     Term.includes(:descriptions).find_each do |term|
-      content = Example.where(description_id: term.descriptions.pluck(:id)).pluck(:content).join
-      file.puts "#{term.name}#{content}"
+      content = Example.where(description_id: term.descriptions.select(:id)).pluck(:content).join
+      file.puts "\ufffa#{term.name}\ufffb#{content}"
     end
   end
 end
 
-# 跑一次大約要 150 秒
+# 跑一次大約要 190 秒
 %w(index_json stem_words_json revdict_amis_def_txt revdict_amis_ex_txt).each do |name|
   eval(name)
 end
