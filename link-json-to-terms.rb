b094e1fc11cebb5e844e1ca890ffc9f31c780c65
wildjcrt <wildjcrt@yahoo.com.tw>
2020-07-04 22:18:12 +0800

    Separate linking terms by including space or not in link-json-to-terms.rb

--- link-json-to-terms.rb
+++ link-json-to-terms.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'json'
 
 require './models/application_record.rb'
@@ -6,14 +7,22 @@ require './models/description'
 require './models/example'
 require './models/synonym'
 
+# 總共大約要 49 分
 index_json = File.read("s/index.json")
 terms = JSON.parse(index_json)
-terms = terms.select {|t| t.length < 25} # 25 字以上先視為 bug
 
-total = terms.size
+terms_with_spaces    = []
+terms_without_spaces = []
+terms.each do |term|
+  if term.include?(' ')
+    terms_with_spaces    << term
+  else
+    terms_without_spaces << term
+  end
+end
 
-# 跑一次大約要 28 分
-terms.each_with_index do |term, i|
+total = terms_with_spaces.size
+terms_with_spaces.each_with_index do |term, i|
   counter = i + 1
   print "\r** << #{format('%5d', counter)} / #{total}, #{format('%.2f', (counter.to_f / total * 100))}% >> **\tID: #{term}"
 
@@ -24,13 +33,48 @@ terms.each_with_index do |term, i|
     Description.where.not(id: current_description_ids)
                .where('content LIKE ?', "%#{term}%")
                .find_each do |description|
-      new_content = description.content.split(' ').map do |part|
-        if part.include?("`") || part.include?("~")
+      new_content = description.content.gsub(/(#{term})/i, '`\1~')
+      description.update(content: new_content)
+    end
+
+    Example.where.not(description_id: current_description_ids)
+           .where('content LIKE ?', "%#{term}%")
+           .find_each do |example|
+      new_content = example.content.gsub(/(#{term})/i, '`\1~')
+      example.update(content: new_content)
+    end
+
+    Synonym.where.not(description_id: current_description_ids)
+           .where('content LIKE ?', "%#{term}%")
+           .find_each do |synonym|
+      new_content = synonym.content.gsub(/(#{term})/i, '`\1~')
+      synonym.update(content: new_content)
+    end
+  end
+end
+puts "done terms_with_spaces"
+
+total = terms_without_spaces.size
+terms_without_spaces.each_with_index do |term, i|
+  counter = i + 1
+  print "\r** << #{format('%5d', counter)} / #{total}, #{format('%.2f', (counter.to_f / total * 100))}% >> **\tID: #{term}"
+
+  current_term_ids = Term.where(lower_name: term).pluck(:id)
+  current_description_ids = Description.where(term_id: current_term_ids).pluck(:id)
+
+  ApplicationRecord.transaction do
+    Description.where.not(id: current_description_ids)
+               .where('content LIKE ?', "%#{term}%")
+               .find_each do |description|
+      new_content = description.content.split(/(`.*?~)/).map do |part|
+        if part.include?("`")
           part
         else
-          part.gsub(/(#{term})/i, '`\1~')
+          part.split(/( )/).map do |fragment|
+            fragment.gsub(/(#{term})/i, '`\1~')
+          end.join
         end
-      end.join(' ')
+      end.join
 
       description.update(content: new_content)
     end
@@ -38,13 +82,15 @@ terms.each_with_index do |term, i|
     Example.where.not(description_id: current_description_ids)
            .where('content LIKE ?', "%#{term}%")
            .find_each do |example|
-      new_content = example.content.split(' ').map do |part|
-        if part.include?("`") || part.include?("~")
+      new_content = example.content.split(/(`.*?~)/).map do |part|
+        if part.include?("`")
           part
         else
-          part.gsub(/(#{term})/i, '`\1~')
+          part.split(/( )/).map do |fragment|
+            fragment.gsub(/(#{term})/i, '`\1~')
+          end.join
         end
-      end.join(' ')
+      end.join
 
       example.update(content: new_content)
     end
@@ -52,13 +98,15 @@ terms.each_with_index do |term, i|
     Synonym.where.not(description_id: current_description_ids)
            .where('content LIKE ?', "%#{term}%")
            .find_each do |synonym|
-      new_content = synonym.content.split(' ').map do |part|
-        if part.include?("`") || part.include?("~")
+      new_content = synonym.content.split(/(`.*?~)/).map do |part|
+        if part.include?("`")
           part
         else
-          part.gsub(/(#{term})/i, '`\1~')
+          part.split(/( )/).map do |fragment|
+            fragment.gsub(/(#{term})/i, '`\1~')
+          end.join
         end
-      end.join(' ')
+      end.join
 
       synonym.update(content: new_content)
     end
